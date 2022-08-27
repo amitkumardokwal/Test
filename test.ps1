dba6137ed54b95d4674b77ec871debcf91ba5191
Amitkumar Dokwal <amitkumardokwal@gmail.com>
2022-08-27 18:40:26 +0200

    Final

--- test.ps1
+++ test.ps1
@@ -1,9 +1,8 @@
 Write-Output "hello world"
-#<<<<<<< HEAD
+
 Write-Output "This is second commit"
-#=======
-Write-Output "This is second commit"
+
 
 #This code is from Branch 02
 Write-Output "This code is coming from Branch 02"
-#>>>>>>> Branch02
+
