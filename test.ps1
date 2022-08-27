8246ee94aae19eeba15df31f105932a8aae5c4a9
Amitkumar Dokwal <amitkumardokwal@gmail.com>
2022-08-27 18:31:14 +0200

    added

--- test.ps1
+++ test.ps1
@@ -1,9 +1,9 @@
 Write-Output "hello world"
-<<<<<<< HEAD
+#<<<<<<< HEAD
 Write-Output "This is second commit"
-=======
+#=======
 Write-Output "This is second commit"
 
 #This code is from Branch 02
 Write-Output "This code is coming from Branch 02"
->>>>>>> Branch02
+#>>>>>>> Branch02
